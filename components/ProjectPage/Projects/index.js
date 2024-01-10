33b4c04ed87fa17ca98fbc367f064b51aeb34d65
nikhilganireddy <nikhilganireddy@yahoo.com>
2024-01-10 14:30:53 +0530

    Commit 11 images location error fix

--- components/ProjectPage/Projects/index.js
+++ components/ProjectPage/Projects/index.js
@@ -1,34 +1,30 @@
-import goku from "public/images/gokuu.png"
-import kuce from "public/images/kuce.png"
-import aifusion from "public/images/aifusion.png"
-
 const projects = [{
     id: 1,
     title: "AI Fusion Toolbox",
     description: "A groundbreaking SaaS platform addressing limitations in ChatGPT. Integrating image generation, video\n" + "creation, and advanced text-based AI, it enhances creative processes with a user-friendly interface. Offering five free AI prompts and a subscription model, the toolbox bridges the gap between human\n" + "creativity and machine intelligence",
     href: "/",
-    image: goku,
+    image: "public/images/gokuu.png",
     skills: ["Next JS", "ReactJs", "Tailwindcss", "MongoDB"]
 }, {
     id: 2,
     title: "AI Fusion Toolbox",
     description: "A groundbreaking SaaS platform addressing limitations in ChatGPT. Integrating image generation, video\n" + "creation, and advanced text-based AI, it enhances creative processes with a user-friendly interface. Offering five free AI prompts and a subscription model, the toolbox bridges the gap between human\n" + "creativity and machine intelligence",
     href: "/",
-    image: goku,
+    image: "public/images/gokuu.png",
     skills: ["Next JS", "ReactJs", "Tailwindcss", "MongoDB"]
 }, {
     id: 3,
     title: "AI Fusion Toolbox",
     description: "A groundbreaking SaaS platform addressing limitations in ChatGPT. Integrating image generation, video\n" + "creation, and advanced text-based AI, it enhances creative processes with a user-friendly interface. Offering five free AI prompts and a subscription model, the toolbox bridges the gap between human\n" + "creativity and machine intelligence",
     href: "/",
-    image: aifusion,
+    image: "public/images/aifusion.png",
     skills: ["Next JS", "ReactJs", "Tailwindcss", "MongoDB"]
 }, {
     id: 4,
     title: "KUCE - Univ College of Engg Website",
     description: "A groundbreaking SaaS platform addressing limitations in ChatGPT. Integrating image generation, video\n" + "creation, and advanced text-based AI, it enhances creative processes with a user-friendly interface. Offering five free AI prompts and a subscription model, the toolbox bridges the gap between human\n" + "creativity and machine intelligence",
     href: "/",
-    image: kuce,
+    image: "public/images/kuce.png",
     skills: ["Next JS", "ReactJs", "Tailwindcss", "MongoDB"]
 }]
 
