023d19282baab529953d7a601648392594b02bad
nikhilganireddy <nikhilganireddy@yahoo.com>
2024-01-14 10:08:34 +0530

    Commit 14

--- components/ProjectPage/Projects/index.js
+++ components/ProjectPage/Projects/index.js
@@ -1,21 +1,12 @@
 const projects = [{
     id: 1,
-    title: "AI Fusion Toolbox",
+    title: "Threads App",
     description: "A groundbreaking SaaS platform addressing limitations in ChatGPT. Integrating image generation, video\n" + "creation, and advanced text-based AI, it enhances creative processes with a user-friendly interface. Offering five free AI prompts and a subscription model, the toolbox bridges the gap between human\n" + "creativity and machine intelligence",
     href: "/",
     image: "https://raw.githubusercontent.com/NikhilGanireddy/portvolio-v2/main/public/images/gokuu.png",
     height: "5120",
     width: "2880",
-    skills: ["Next JS", "ReactJs", "Tailwindcss", "MongoDB"]
-}, {
-    id: 2,
-    title: "AI Fusion Toolbox",
-    description: "A groundbreaking SaaS platform addressing limitations in ChatGPT. Integrating image generation, video\n" + "creation, and advanced text-based AI, it enhances creative processes with a user-friendly interface. Offering five free AI prompts and a subscription model, the toolbox bridges the gap between human\n" + "creativity and machine intelligence",
-    href: "/",
-    image: "https://raw.githubusercontent.com/NikhilGanireddy/portvolio-v2/main/public/images/gokuu.png",
-    height: "5120",
-    width: "2880",
-    skills: ["Next JS", "ReactJs", "Tailwindcss", "MongoDB"]
+    skills: ["Next JS", "ReactJs", "Tailwindcss", "MongoDB", "Typescript", "Clerk"]
 }, {
     id: 3,
     title: "AI Fusion Toolbox",
@@ -24,16 +15,34 @@ const projects = [{
     image: "https://raw.githubusercontent.com/NikhilGanireddy/portvolio-v2/main/public/images/aifusion.png",
     height: "2560",
     width: "1514",
-    skills: ["Next JS", "ReactJs", "Tailwindcss", "MongoDB"]
+    skills: ["Next JS", "ReactJs", "Tailwindcss", "Clerk"]
 }, {
     id: 4,
     title: "KUCE - Univ College of Engg Website",
-    description: "A groundbreaking SaaS platform addressing limitations in ChatGPT. Integrating image generation, video\n" + "creation, and advanced text-based AI, it enhances creative processes with a user-friendly interface. Offering five free AI prompts and a subscription model, the toolbox bridges the gap between human\n" + "creativity and machine intelligence",
+    description: "A groundbreaking SaaS platform addressing limitations in ChatGPT. Integrating image generation, video\n" + "creation, and advanced text-based AI, it enhances creative processes with a user-friendly interface. Offering five free AI prompts and a subscription model, the toolbox bridges the gap 5between human\n" + "creativity and machine intelligence",
     href: "https://kuce.vercel.app",
     image: "https://raw.githubusercontent.com/NikhilGanireddy/portvolio-v2/main/public/images/kuce.png",
     height: "2532",
     width: "1524",
-    skills: ["Next JS", "ReactJs", "Tailwindcss", "MongoDB"]
-}]
+    skills: ["Next JS", "ReactJs", "Tailwindcss", "MongoDB", "NextUI"]
+}, {
+    id: 5,
+    title: "Rudramadevi - KUCE Hostel Management App",
+    description: "A groundbreaking SaaS platform addressing limitations in ChatGPT. Integrating image generation, video\n" + "creation, and advanced text-based AI, it enhances creative processes with a user-friendly interface. Offering five free AI prompts and a subscription model, the toolbox bridges the gap 5between human\n" + "creativity and machine intelligence",
+    href: "https://token-server.netlify.app",
+    image: "https://raw.githubusercontent.com/NikhilGanireddy/portvolio-v2/main/public/images/kuce.png",
+    height: "2532",
+    width: "1524",
+    skills: ["Next JS", "ReactJs", "Tailwindcss", "MongoDB", "Next Auth"]
+}, {
+    id: 2,
+    title: "thegdp - Blog",
+    description: "A groundbreaking SaaS platform addressing limitations in ChatGPT. Integrating image generation, video\n" + "creation, and advanced text-based AI, it enhances creative processes with a user-friendly interface. Offering five free AI prompts and a subscription model, the toolbox bridges the gap between human\n" + "creativity and machine intelligence",
+    href: "/",
+    image: "https://raw.githubusercontent.com/NikhilGanireddy/portvolio-v2/main/public/images/gokuu.png",
+    height: "5120",
+    width: "2880",
+    skills: ["Next JS", "ReactJs", "Tailwindcss", "MongoDB", "Typescript", "Clerk", "trpc"]
+},]
 
 export default projects
